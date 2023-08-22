0d0e16a5521132d85f3212ccfa691f782e1b2699
ilnur khabibrakhmanov <gs1.ilnur@gmail.com>
2023-08-22 20:52:33 +0300

    create attribute self.view_count

--- src/channel.py
+++ src/channel.py
@@ -17,6 +17,7 @@ class Channel:
         self.url = f'https://www.youtube.com/channel/{self.channel_id}'
         self.subscriber_count = self.channel['items'][0]['statistics']['subscriberCount']
         self.video_count = self.channel['items'][0]['statistics']['videoCount']
+        self.view_count = self.channel['items'][0]['statistics']['viewCount']
 
     @property
     def channel_id(self):
