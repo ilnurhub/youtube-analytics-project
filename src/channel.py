2b21a8d92a87ba54054ff207610cd95af0db6ccd
ilnur khabibrakhmanov <gs1.ilnur@gmail.com>
2023-08-22 21:06:41 +0300

    create class-method get_service()

--- src/channel.py
+++ src/channel.py
@@ -8,10 +8,8 @@ class Channel:
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        api_key = os.getenv('YT_API_KEY')
-        self.youtube = build('youtube', 'v3', developerKey=api_key)
         self.__channel_id = channel_id
-        self.channel = self.youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
+        self.channel = Channel.get_service().channels().list(id=self.channel_id, part='snippet,statistics').execute()
         self.title = self.channel['items'][0]['snippet']['title']
         self.description = self.channel['items'][0]['snippet']['description']
         self.url = f'https://www.youtube.com/channel/{self.channel_id}'
@@ -26,3 +24,12 @@ class Channel:
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
         print(json.dumps(self.channel, indent=2, ensure_ascii=False))
+
+    @classmethod
+    def get_service(cls):
+        """
+        Возвращает объект для работы с YouTube API
+        """
+        api_key = os.getenv('YT_API_KEY')
+        youtube = build('youtube', 'v3', developerKey=api_key)
+        return youtube
