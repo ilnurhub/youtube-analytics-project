32127dd8dd7fff51a4cc748fb37674ec6c0860fa
ilnur khabibrakhmanov <gs1.ilnur@gmail.com>
2023-08-24 22:19:01 +0300

    add def __sub__()

--- src/channel.py
+++ src/channel.py
@@ -34,6 +34,12 @@ class Channel:
         """
         return int(self.subscriber_count) + int(other.subscriber_count)
 
+    def __sub__(self, other):
+        """
+        Возвращает результат вычитания количества подписчиков двух каналов
+        """
+        return int(self.subscriber_count) - int(other.subscriber_count)
+
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
         print(json.dumps(self.channel, indent=2, ensure_ascii=False))
