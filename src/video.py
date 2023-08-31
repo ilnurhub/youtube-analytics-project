c928d6a0fc5f2aab1b0dcdc85bc84b3aba78bf11
ilnur khabibrakhmanov <gs1.ilnur@gmail.com>
2023-08-31 22:07:07 +0300

    add def __init__()

--- src/video.py
+++ src/video.py
@@ -1,2 +1,12 @@
-class Video:
-    pass
+from src.channel import Channel
+
+
+class Video(Channel):
+    def __init__(self, video_id: str):
+        self.video_id = video_id
+        self.video_response = self.get_service().videos().list(part='snippet,statistics,contentDetails,topicDetails',
+                                                               id=self.video_id).execute()
+        self.video_title = self.video_response['items'][0]['snippet']['title']
+        self.video_link = f'https://www.youtube.com/watch?v={self.video_id}'
+        self.view_count = self.video_response['items'][0]['statistics']['viewCount']
+        self.like_count = self.video_response['items'][0]['statistics']['likeCount']
