4794bd0dbc7477a59f9d9b04958ab8f41593673f
ilnur khabibrakhmanov <gs1.ilnur@gmail.com>
2023-09-03 21:22:05 +0300

    add def __init__()

--- src/playlist.py
+++ src/playlist.py
@@ -1,2 +1,4 @@
 class PlayList:
-    pass
+    def __init__(self, playlist_id):
+        self.playlist_id = playlist_id
+        self.url = f'https://www.youtube.com/playlist?list={self.playlist_id}'
